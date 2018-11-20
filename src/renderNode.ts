8c60d422aad69b1f5c1d44e700b0578be999fc34
Oliver <rift95@live.se>
2018-11-20 15:34:20 +0100

    fixed ts type error and renamed nodeTree to node

--- src/renderNode.ts
+++ src/renderNode.ts
@@ -1,23 +1,23 @@
 import { VNode } from "./util/vnode";
 
-export function renderNode(nodeTree: VNode): HTMLElement {
-    const elem = document.createElement(nodeTree.tag);
-    if (nodeTree.value) {
-        elem['value'] = nodeTree.value;
-        elem.setAttribute('value', '' + nodeTree.value);
+export function renderNode(node: VNode): HTMLElement {
+    const elem = document.createElement(node.tag);
+    if (node.value) {
+        elem['value'] = node.value;
+        elem.setAttribute('value', '' + node.value);
     }
-    elem.innerText = nodeTree.text;
-    Object.keys(nodeTree.props).forEach(prop => {
-        elem.setAttribute(prop, nodeTree.props[prop]);
+    elem.innerText = node.text;
+    Object.keys(node.props).forEach(prop => {
+        elem.setAttribute(prop, node.props[prop]);
     });
-    Object.keys(nodeTree.events).forEach(event => {
+    Object.keys(node.events).forEach(event => {
         elem.addEventListener(event, e => {
-            nodeTree.events[event].forEach(cb => {
+            node.events[event].forEach(cb => {
                 cb(e);
             });
         });
     });
-    nodeTree.children.forEach(node => {
+    node.children.forEach(node => {
         elem.appendChild(renderNode(node));
     });
     return elem;
