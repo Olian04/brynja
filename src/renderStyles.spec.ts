6735c363670a701ab8ff7118a1953794302e7085
Oliver <rift95@live.se>
2019-10-08 15:40:12 +0200

    implemented renderStyles

---
+++ src/renderStyles.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import jsdom from 'mocha-jsdom';
+
+import { renderStyle } from './renderStyles';
+
+describe('renderStyle', () => {
+  jsdom();
+
+  it('typecheck', () => {
+    expect(typeof renderStyle).to.equal('function');
+  });
+
+  it('single property applied once', () => {
+    const inputStyle = {
+      'some-class': {
+        background: 'orangered',
+      },
+    };
+
+    const $styles = renderStyle(inputStyle);
+    const styles = $styles.textContent.replace(/\s/g, '');
+    expect(styles).to.equal('.some-class{background:orangered;}');
+  });
+
+  it('single pseudo selector applied once', () => {
+    const inputStyle = {
+      'some-class': {
+        ':hover': {
+          background: 'orangered',
+        },
+      },
+    };
+
+    const $styles = renderStyle(inputStyle);
+    const styles = $styles.textContent.replace(/\s/g, '');
+    expect(styles).to.equal('.some-class:hover{background:orangered;}');
+  });
+});
