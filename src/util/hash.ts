616c626fcb5c60f5143f5a2143387b88617467c1
Oliver Anteros <oliverli@kth.se>
2022-03-17 10:32:38 +0100

    chore: Allow bitwise operation in hash.ts

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:no-bitwise */
+
 /**
  * Source: https://gist.github.com/jlevy/c246006675becc446360a798e2b2d781
  * This is a simple, *insecure* hash that's short, fast, and has no dependencies.
